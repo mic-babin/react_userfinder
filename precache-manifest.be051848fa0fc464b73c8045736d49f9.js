fa22e621b443cbff05439ca4bc04c7aa5b6f5d32
mic-babin <61392281+mic-babin@users.noreply.github.com>
2020-05-02 09:36:15 -0400

    first commit

---
+++ precache-manifest.be051848fa0fc464b73c8045736d49f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd80e42a446d7c1716b3f66463887774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3538cadfb71d784f5b9d",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "/static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "3538cadfb71d784f5b9d",
+    "url": "/static/js/main.562773d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
